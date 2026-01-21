f9d3e2527fedb925905798a039cfdac485affa0c
MiMendiola <mglmendiol@gmail.com>
2026-01-21 02:13:16 -0800

    Uploading cpp06

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -36,7 +36,7 @@ void identify(Base& p)
     try
     {
         (void)dynamic_cast<A&>(p);
-        std::cout << "Type A" << std::endl;
+        std::cout << "Type A\n" << std::endl;
         return ;
     }
     catch(const std::exception& e)
@@ -44,7 +44,7 @@ void identify(Base& p)
         try
         {
             (void)dynamic_cast<B&>(p);
-            std::cout << "Type B" << std::endl;
+            std::cout << "Type B\n" << std::endl;
             return ;
         }
         catch(const std::exception& e)
@@ -52,12 +52,12 @@ void identify(Base& p)
             try
             {
                 (void)dynamic_cast<C&>(p);
-                std::cout << "Type C" << std::endl;
+                std::cout << "Type C\n" << std::endl;
                 return ;
             }
             catch(const std::exception& e)
             {
-                std::cout << "Not identified" << std::endl;
+                std::cout << "Not identified\n" << std::endl;
             }
         }
     }
@@ -66,16 +66,16 @@ void identify(Base& p)
 int main()
 {
     srand(static_cast<unsigned int>(time(NULL)));
+
+    for (int i = 1; i < 11 ; i++)
     {
         Base* base = generate();
-        /* Base* base = new Base(); */
-
+        std::cout << "Base: " << i << std::endl;
         identify(base);
-
         identify(*base);
-
         delete base;
     }
+
 
     return 0;
 }
